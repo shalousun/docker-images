bbc7e157f7770e519019e33aaf40f999a60a0bc4
oppofind <836575280@qq.com>
2019-03-23 02:15:58 +0800

    修改用户

--- kibana-xpack-less/scripts/kibana.sh
+++ kibana-xpack-less/scripts/kibana.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -o nounset # Treat unset variables as an error
-exec su -l kibana -s /bin/bash -c "/usr/local/kibana/bin/kibana -c /etc/kibana/kibana.yml"
+exec su -l root -s /bin/bash -c "/usr/local/kibana/bin/kibana -c /etc/kibana/kibana.yml"
