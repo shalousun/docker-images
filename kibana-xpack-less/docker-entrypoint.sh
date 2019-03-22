e822d88b56cde40ab4897c968e77e7f7f72ce09a
oppofind <836575280@qq.com>
2019-03-23 03:05:44 +0800

    添加kibana-xpack-less

---
+++ kibana-xpack-less/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+PATH=$PATH":/usr/share/kibana/bin"
+# Add kibana as command if needed
+if [[ "$1" == -* ]]; then
+	set -- kibana "$@"
+fi
+
+# Run as user "kibana" if the command is "kibana"
+if [ "$1" = 'kibana' ]; then
+	set -- su - kibana -s /bin/sh -c kibana -- "$@"
+fi
+exec "$@"
